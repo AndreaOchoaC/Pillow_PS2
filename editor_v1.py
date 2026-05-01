0f07d2889fbabf4409b9c83596ed3ca82e3e6df0
Andrea Ochoa Córdova <91387374+AndreaOchoaC@users.noreply.github.com>
2026-05-01 13:08:00 -0600

    corregir error
    Fix variable name case for image label in display_image function.

--- editor_v1.py
+++ editor_v1.py
@@ -1,7 +1,10 @@
-# copia esta función 
+# El problema con tu función era que definiste la etiqueta "image_Label" con L mayúscula
+# y en la función está con l minúscula
+# ya debería funcionar bien
+
 def display_image(self, img):
     img = img.convert("RGB")
-    max_width, max_height = self.image_label.width(), self.image_label.height()
+    max_width, max_height = self.image_Label.width(), self.image_Label.height()
     img = img.copy()
     img.thumbnail((max_width, max_height), Image.LANCZOS)
     data = img.tobytes("raw", "RGB")
